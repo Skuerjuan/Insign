29cf9c15959bad8759550ee93e34092a9c730804
tobiasgerman <>
2026-05-27 12:07:54 -0300

    seguir pantalla menu

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,7 +1,25 @@
+import styles from "./styles.module.css";
+import Link from "next/link";
+import Input from "@/components/input";
+
+
 export default function Menu() {
   return (
-    <div>
-      <h1>aieka</h1>
+    <div className={styles.fondo}>
+      <aside className={styles.aside}>
+       <h1 className={styles.logo}>
+        <span className={styles.blanco}>In</span>
+        <span className={styles.amarillo}>Sign</span>
+      </h1>
+       <Link href="./" className={styles.inicio}>
+       <img src="casa.png" className={styles.casa} />
+       Inicio
+       </Link>
+       <Link href="./" className={styles.train}>
+       <img src="pesa.png" className={styles.pesa}/>
+       Entrenamiento
+       </Link> 
+      </aside>
     </div>
-  );
+    );
 }
